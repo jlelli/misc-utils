a1548f99faf73905432c5acb9559e8711794df92
Juri Lelli <juri.lelli@gmail.com>
2014-03-27 16:29:51 +0100

    ssh-vm: check number of parameters

--- ssh-vm.sh
+++ ssh-vm.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 # Connect to virtual machines via ssh
 
+if [ "$#" -ne 1 ]; then
+    echo "guest not specified, valid guests are:"
+    echo "guest1-debian-64"
+    echo "guest2-debian-64"
+    echo "usage: ssh-vm [guest_name]"
+    exit 0
+fi
+
 VMNAME=$1
 
 ssh $(ssh-vm.py $1) 2> /dev/null
